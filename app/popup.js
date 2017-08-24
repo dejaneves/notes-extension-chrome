e53e90e477250bf822fff318e000ffd6ddec0982
Jaime Neves <jaimebarrosoneves@gmail.com>
2017-08-23 22:59:39 -0400

    in progress

--- app/popup.js
+++ app/popup.js
@@ -1,8 +1,8 @@
 
 (function(){
-  
+
   'use strict';
-  
+
   function getNotes() {
     var notes = [
       {
@@ -16,26 +16,27 @@
         description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
       },
       {
+        id:'_6234246',
         title : 'Lorem ipsum dolor sit amet.',
         description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
       }
     ];
     return notes;
   }
-  
+
   function $id(id) {
     return document.getElementById(id);
   }
-  
+
   function LoadHTML(url, id) {
     var req = new XMLHttpRequest();
     req.open('GET', url);
     req.send();
     req.onload = () => {
-      $id(id).innerHTML = req.responseText; 
+      $id(id).innerHTML = req.responseText;
     }
   }
-  
+
   function BindEvent() {
     var root = null;
     var useHash = true;
@@ -43,22 +44,22 @@
     var router = new Navigo(root, useHash, hash);
 
     router
-    .on('/note/:id', function () {
-      console.log('here');
+    .on({
+      '/note/:id' : function(params) {
+        console.log('here 1', params);
+      }
     })
     .resolve();
   }
 
-  
+
   document.addEventListener('DOMContentLoaded', function () {
     BindEvent();
-    var list = ''; var notes = getNotes(); 
+    var list = ''; var notes = getNotes();
     for (var i = 0; i < notes.length; i++) {
       list += '<li> <a href="#!/note/ '+notes[i].id+' "> ' + notes[i].title + '</a> </li>';
     }
     document.querySelector('ul').innerHTML = list;
   });
-  
+
 })();
-
-
